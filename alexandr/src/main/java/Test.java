ab755364242af3def34ea05a7d32354e1048c20e
xenc <sasha.xenc@gmail.com>
2017-09-20 20:32:24 +0300

    Alexandet first commit. Add rectangle

--- alexandr/src/main/java/Test.java
+++ alexandr/src/main/java/Test.java
@@ -1,2 +1,7 @@
 public class Test {
+    public static void main(String args[]){
+        System.out.println("!!!!!!!!!!!");
+        System.out.println("!!!     !!!");
+        System.out.println("!!!!!!!!!!!");
+    }
 }

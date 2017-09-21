e2abe2b0bfb3674ddfad68df95a6752399dc4058
AndriyYermolenko <andrya@ukr.net>
2017-09-22 01:39:06 +0300

    ClassWork 1

---
+++ andrey/src/main/java/Cw1.java
@@ -0,0 +1,27 @@
+public class Cw1 {
+    public static void main(String[] args) {
+      int x=43;
+      int a1, a2, a3, a4, a5, a6;
+      a1=x%2;
+      x=x/2;
+      a2=x%2;
+      x=x/2;
+      a3=x%2;
+      x=x/2;
+      a4=x%2;
+      x=x/2;
+      a5=x%2;
+      x=x/2;
+      a6=x%2;
+        x=x/2;
+
+        System.out.print(a6);
+        System.out.print(a5);
+        System.out.print(a4);
+        System.out.print(a3);
+        System.out.print(a2);
+        System.out.print(a1);
+
+    }
+
+}

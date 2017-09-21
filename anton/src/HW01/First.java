2a0bbabba36148d46104731dba358ce5b3fe63a8
Antoniuk Anton <antonantonuk1999@gmail.com>
2017-09-21 17:52:00 +0300

    HW01

---
+++ anton/src/HW01/First.java
@@ -0,0 +1,63 @@
+public static class{
+    public static void main(String[]argz){
+//1
+        byte a;
+        short b;
+        int c;
+        long d;
+//2
+        byte e = 5 + 10;
+
+        System.out.println("2. e = " + e);
+//3
+        short e = 636 - 353;
+
+        System.out.println("3. e = " + e);
+//4
+        int i = 56486/554;
+
+        System.out.println("4. i = " + i);
+//5
+        long j = 45675*354;
+
+        System.out.println("5. j = " j);
+//6
+        byte a6 = 5;
+        byte b6 = 74;
+        byte c = a6 + b6;
+
+        System.out.println("6. c = " + c);
+//7
+        short a7 = 67;
+        short b7 = 867;
+        byte c = 7(b7 + a7)/a6;
+
+        System.out.println("7. c = " + c);
+//8
+        int a8 = (c7/c6)*(b7/a7);
+
+        System.out.println("8. a = " + a8);
+//9
+        int g = 4846;
+        int h = 9366;
+        int k;
+
+        System.out.println("9. g = " + g + " h = " + h);
+
+        k = h;
+        h = g;
+        g = k;
+
+        System.out.println(" g = " + g + " h = " + h);
+//10
+        int x = 47;
+        int y = 485;
+
+        System.out.println("10.  x = " + x + " y = " + y);
+        x = x*y;
+        y = x/y;
+        x = x/y;
+
+        System.out.println(" x = " + x + " y = " + y);
+    }
+}

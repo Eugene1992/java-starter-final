23d1d137fe10c209211622b45bed40eb1bf1333b
Rudakevych <yurij.rudakevych@gmail.com>
2017-09-21 21:10:51 +0300

    Added ClassWork1 class (From number to binary)

--- yuriy/src/main/java/ClassWork1.java
+++ yuriy/src/main/java/ClassWork1.java
@@ -1,6 +1,46 @@
-/**
- * Created by Student on 9/21/2017.
- */
+
 public class ClassWork1 {
+    public static void main(String[] args) {
+
+        int ourNumber = 125;
+
+        int num1 = ourNumber / 2;
+        int value1 = num1 % 2;
+        System.out.print(value1);
+
+        int num2 = num1 / 2;
+        int value2 = num2 % 2;
+        System.out.print(value2);
+
+        int num3 = num2 / 2;
+        int value3 = num3 % 2;
+        System.out.print(value3);
+
+        int num4 = num3 / 2;
+        int value4 = num4 % 2;
+        System.out.print(value4);
+
+        int num5 = num4 / 2;
+        int value5 = num5 % 2;
+        System.out.print(value5);
+
+        int num6 = num5 / 2;
+        int value6 = num5 % 2;
+        System.out.print(value6);
+
+        int num7 = num6 / 2;
+        int value7 = num6 % 2;
+        System.out.print(value7);
+
+//        int num8 = num7 / 2;
+//        int value8 = num7 % 2;
+//        System.out.print(value8);
+//
+//        int num9 = num8 / 2;
+//        int value9 = num7 % 2;
+//        System.out.print(value9);
+    }
+
+
 
 }

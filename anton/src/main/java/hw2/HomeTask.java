54193ddcf852f38d980996b6e2ef7b3695ad4cd1
Evgeniy <deyneko55@gmail.com>
2017-09-22 19:10:29 +0300

    Added hw changes

--- anton/src/main/java/hw2/HomeTask.java
+++ anton/src/main/java/hw2/HomeTask.java
@@ -10,18 +10,20 @@ public class HomeTask {
         //2
         System.out.println("2.");
         int firstVal = scan.nextInt();
-        if (firstVal%2==1)
-        { System.out.println("This value is uneven");
-        }else {System.out.println("This value is even");}
+        if (firstVal % 2 == 1) {
+            System.out.println("This value is uneven");
+        } else {
+            System.out.println("This value is even");
+        }
 
         //4
         System.out.println("4.");
         int fourthVal = 574;
         int fifthsVal = 24;
         int sixthVal = 486;
-        int answer ;
-        answer = fourthVal>fifthsVal ? fifthsVal : fourthVal ;
-        answer = answer>sixthVal ? sixthVal : answer ;
+        int answer;
+        answer = fourthVal > fifthsVal ? fifthsVal : fourthVal;
+        answer = answer > sixthVal ? sixthVal : answer;
         System.out.println(answer);
     }
 }

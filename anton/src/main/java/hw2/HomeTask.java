14e64da285b5a4385177fb1328a66aaa3059972a
Antoniuk Anton <antonantonuk1999@gmail.com>
2017-09-22 19:08:43 +0300

    hw02.01

--- anton/src/main/java/hw2/HomeTask.java
+++ anton/src/main/java/hw2/HomeTask.java
@@ -7,8 +7,21 @@ public class HomeTask {
 
         Scanner scan = new Scanner(System.in);
 
-        String str = scan.next();
+        //2
+        System.out.println("2.");
+        int firstVal = scan.nextInt();
+        if (firstVal%2==1)
+        { System.out.println("This value is uneven");
+        }else {System.out.println("This value is even");}
 
-        int i = scan.nextInt();
+        //4
+        System.out.println("4.");
+        int fourthVal = 574;
+        int fifthsVal = 24;
+        int sixthVal = 486;
+        int answer ;
+        answer = fourthVal>fifthsVal ? fifthsVal : fourthVal ;
+        answer = answer>sixthVal ? sixthVal : answer ;
+        System.out.println(answer);
     }
 }

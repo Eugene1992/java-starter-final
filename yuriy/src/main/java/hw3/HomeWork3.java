3c825764d2c14b434e6ac3b8a23a63edebdff31d
Rudakevych <yurij.rudakevych@gmail.com>
2017-09-23 22:19:26 +0300

    Added class work 4, 5

--- yuriy/src/main/java/hw3/HomeWork3.java
+++ yuriy/src/main/java/hw3/HomeWork3.java
@@ -18,7 +18,44 @@ public class HomeWork3 {
 //            System.out.println(b);
 //        }
 //
-        System.out.println("4. Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….");
+//        System.out.println("4. Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….");
+//        int valueHw3Task4 = 2;
+//        System.out.println(valueHw3Task4);
+//
+//        for(int c = 0; c <= 20; c++){
+//            valueHw3Task4 = valueHw3Task4 * 2;
+//            System.out.println(valueHw3Task4);
+//        }
+//
+//        System.out.println("5. Выведите на экран первые 20 членов последовательности Фибоначчи.");
+//        int fibonacciNumberCount = 20;
+//        int previousNumber = 0;
+//        int nextNumber = 1;
+//
+//        System.out.println(previousNumber);
+//        System.out.println(nextNumber);
+//
+//        for(int i = 2; i < fibonacciNumberCount; i++){
+//            int fibonacciNumber = previousNumber + nextNumber;
+//
+//            System.out.println(fibonacciNumber);
+//
+//            previousNumber = nextNumber;
+//            nextNumber = fibonacciNumber;
+//        }
+//
+        /**
+//         * Написать программу, которая реализует следующий функционал:
+//         6.1 Пользователь вводит число в 10-ой системе счисления.
+//         6.2 Программа выводит сообщение в котором предлагает пользователю перевести данное число в одну из систем счисления на выбор:
+//         в 2-ую
+//         в 8-ую
+//         в 16-ую
+//         6.3 Пользователь выбирает один из предложенных вариантов, программа выполняет соответствующий перевод числа и выводит
+//         на экран результат.
+//         */
+
+
 
     }
 }

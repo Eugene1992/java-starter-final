d2bd790eeb815389ddfe60abdfd13d532a818c69
Antoniuk Anton <antonantonuk1999@gmail.com>
2017-09-26 21:02:54 +0300

    cw04

---
+++ anton/src/main/java/cw4/FactRecurs.java
@@ -0,0 +1,19 @@
+package cw4;
+
+import java.util.Scanner;
+
+public class FactRecurs {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(fact(n, 1));
+    }
+
+    static int fact(int n, int result) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1, result);
+    }
+}

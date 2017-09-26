db9981c86dbb2b2eed0b25b569d95629db64d098
xenc <sasha.xenc@gmail.com>
2017-09-26 20:49:45 +0300

    Alexander ClassWork3 added fix class palindrom

--- alexandr/src/main/java/CalculatePalindromsInInterval.java
+++ alexandr/src/main/java/CalculatePalindromsInInterval.java
@@ -1,6 +1,6 @@
 public class CalculatePalindromsInInterval {
     public static void main(String[] args) {
-        System.out.println(palindromOk(999999));
+        System.out.println(palindromOk(123321));
         int k=0;
         for (int i = 1; i <= 999999; i++){
             if (palindromOk(i)) {k++;}
@@ -13,6 +13,7 @@ public class CalculatePalindromsInInterval {
         String reversS = "";
         for (int i = 0; i < primeS.length(); i++){
             reversS = reversS + n % 10;
+            n/=10;
         }
         if (primeS.equals(reversS)) { return true;}
         else {return false;}

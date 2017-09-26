3b8e6a9e04f2ccee565d5079f0863860f96404b9
Antoniuk Anton <antonantonuk1999@gmail.com>
2017-09-26 16:56:37 +0300

    hw03.02

--- anton/src/main/java/hw2/Interval.java
+++ anton/src/main/java/hw2/Interval.java
@@ -18,13 +18,22 @@ public class Interval {
 
         Scanner scan = new Scanner(System.in);
 
+        int value = scan.nextInt();
 
-
-
-
-
-
-
-
+        if (value <= 0 || value >= 100) {
+            System.out.println("Wrong number");
+        }
+        if (value >= 0 && value <= 14) {
+            System.out.println("Diapasone is [0 - 14]");
+        }
+        if (value >= 15 && value <= 35) {
+            System.out.println("Diapasone is [15 - 35]");
+        }
+        if (value >= 36 && value <= 50) {
+            System.out.println("Diapasone is [36 - 50]");
+        }
+        if (value >=     51 && value <= 100) {
+            System.out.println("Diapasone is [51 - 100]");
+        }
     }
 }

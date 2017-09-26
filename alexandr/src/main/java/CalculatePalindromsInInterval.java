76905ee5f76806d95ae33a94334d07e7c9679876
xenc <sasha.xenc@gmail.com>
2017-09-26 20:47:47 +0300

    Alexander ClassWork3 added new class palindrom

---
+++ alexandr/src/main/java/CalculatePalindromsInInterval.java
@@ -0,0 +1,20 @@
+public class CalculatePalindromsInInterval {
+    public static void main(String[] args) {
+        System.out.println(palindromOk(999999));
+        int k=0;
+        for (int i = 1; i <= 999999; i++){
+            if (palindromOk(i)) {k++;}
+        }
+        System.out.println(k);
+    }
+
+    static boolean palindromOk (int n){
+        String primeS = Integer.toString(n);
+        String reversS = "";
+        for (int i = 0; i < primeS.length(); i++){
+            reversS = reversS + n % 10;
+        }
+        if (primeS.equals(reversS)) { return true;}
+        else {return false;}
+    }
+}

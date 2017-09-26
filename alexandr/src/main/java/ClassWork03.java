a12cb01eff0e0c7075a753363b924e8ab52db863
xenc <sasha.xenc@gmail.com>
2017-09-26 19:38:38 +0300

    Alexander ClassWork3 added method sumOfInterval

---
+++ alexandr/src/main/java/ClassWork03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ClassWork03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the beginning of interval");
+        int beginOfInterval = scan.nextInt();
+        System.out.println("Enter the ending of interval");
+        int endOfInterval = scan.nextInt();
+        System.out.println(sumOfInterval(beginOfInterval,endOfInterval));
+    }
+
+    static int sumOfInterval (int startInterval, int endInterval) {
+        int sum = 0;
+        for (int i = startInterval; i < endInterval; i++) {
+           sum+=i; 
+        }
+        return sum;
+    }
+}

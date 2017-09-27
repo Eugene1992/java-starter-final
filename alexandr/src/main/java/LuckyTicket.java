fe09d0fe60e4cc780ee6c3752ca4dfa7c65001ec
xenc <sasha.xenc@gmail.com>
2017-09-27 20:23:24 +0300

    Alexander ClassWork4 added tasks with array

---
+++ alexandr/src/main/java/LuckyTicket.java
@@ -0,0 +1,16 @@
+public class LuckyTicket {
+    public static void main(String[] args) {
+
+    }
+
+//    static boolean luckyNumber(int n){
+//        String numberString = Integer.toString(n);
+//        int firstHalfSum =0;
+//        String reversS;
+//        for (int i = 0; i < numberString.length() / 2; i++){
+//            reversS = reversS + n % 10;
+//            n/=10;
+//        }
+//
+//    }
+}

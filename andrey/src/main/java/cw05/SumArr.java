43be74feb3e8ee49e1c0c7ca9652650b8e6c81bb
AndriyYermolenko <andrya@ukr.net>
2017-10-01 01:55:44 +0300

    HomerWork04

--- andrey/src/main/java/cw05/SumArr.java
+++ andrey/src/main/java/cw05/SumArr.java
@@ -3,27 +3,27 @@ package cw05;
 public class SumArr {
     public static void main(String[] args) {
 
-
+        int sum = 0;
+        int x = 5, y = 4; //задается размер массива
         int arr[][] = {
-                {1, 3, 4, 5},
-                {4, 5, 6, 7},
-                {9, 8, 6, 4},
-                {2, 6, 9, 0}
-
+                {5, 1, 1, 5,1},
+                {1, 5, 6, 1,1},
+                {1, 8, 6, 4,1},
+                {2, 1, 8, 0,1}
         };
-        for (int i = 0; i <arr.length ; i++) {
-            System.out.println(arr[i][0]);
-        }
-        System.out.println(sumArr(arr));
+        System.out.println("Sum of numbers: "+sumArr(arr,x, y));
     }
 
-    static int sumArr(int a[][]) {
-        int x = 0;
-        for (int i = 0; i < a.length; i++) {
-            x = x + a[i][0];
-
+    static int sumArr(int a[][], int x, int y) {
+        int sum = 0;
+        for (int i = 0; i < x; i++) {
+            sum = sum + a[0][i] + a[y - 1][i];
         }
-        return x;
+        for (int i = 0; i < y; i++) {
+            sum = sum + a[i][x - 1]+a[i][0];
+        }
+        sum = sum - a[0][0] - a[0][x - 1] - a[y - 1][0] - a[y - 1][x - 1];
+        return sum;
     }
 }
 

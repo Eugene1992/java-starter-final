7bbdcf61b2785e9d345440cf6c8a7dce4f8402c2
yr <yr@playtini.ua>
2017-10-04 17:04:08 +0300

    Added test 1

---
+++ yuriy/src/test/java/CalcSummOfAllElementsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class CalcSummOfAllElementsTest {
+
+
+    @Test
+    public void isSummOfAllElementsBiggerThen100500Test(){
+        System.out.println("Test");
+
+    }
+}
